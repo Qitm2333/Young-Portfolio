3be2d78b7bd6a59123316b5fa2f61cdc3134ca09
Qitm2333 <99474511+Qitm2333@users.noreply.github.com>
2025-12-15 14:52:57 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: process.env.NODE_ENV === 'production' ? '/Young-Portfolio/' : '/',
+      base: mode === 'production' ? '/Young-Portfolio/' : '/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
